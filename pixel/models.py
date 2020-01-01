8067ea46d5b52033c926ea56547100702cb2532b
gian <gian2dchris@gmail.com>
2020-01-02 00:09:14 +0200

    tracking slugs and domain selection in homepage

--- pixel/models.py
+++ pixel/models.py
@@ -25,7 +25,7 @@ class PageVisit(models.Model):
 	country_code = models.CharField(max_length=200)
 	region_name = models.CharField(max_length=200)
 	time_opened = models.DateTimeField('time_opened')
-#	domain = models.ForeignKey(Domain,on_delete=models.CASCADE)
+	domain = models.ForeignKey(Domain, on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.ip + self.time_opened
